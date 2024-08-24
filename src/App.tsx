58e968510ec440a049c660ddb93a96259e98e2b3
Yan Turnt <bx6xbChosen1@gmail.com>
2024-08-24 23:45:04 +0500

    - created app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import Canvas from "./components/Canvas"
+import SettingBar from "./components/SettingBar"
+import Toolbar from "./components/Toolbar"
+import "./style/app.scss"
+
 function App() {
-  return <>App</>
+  return (
+    <div className="app">
+      <Toolbar />
+      <SettingBar />
+      <Canvas />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/SettingBar.tsx
@@ -0,0 +1,7 @@
+import "../style/setting-bar.scss"
+
+const SettingBar = () => {
+  return <div className="setting-bar"></div>
+}
+
+export default SettingBar
