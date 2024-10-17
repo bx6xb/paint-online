cefe6f304de7e1ac6fe9ab6407b6118c41aec74c
Yan Turnt <bx6xbChosen1@gmail.com>
2024-10-17 11:26:05 +0500

    reduced code in circle tool

--- src/tools/Circle.ts
+++ src/tools/Circle.ts
@@ -1,37 +1,14 @@
-import Tool from "./Tool"
+import Rect from "./Rect"
 
-export default class Circle extends Tool {
-  mouseDown = false
-  startX = 0
-  startY = 0
-  saved = ""
-
+export default class Circle extends Rect {
   constructor(canvas: HTMLCanvasElement) {
     super(canvas)
-    this.listen()
   }
 
-  listen() {
-    this.canvas.onmouseup = this.mouseUpHandler.bind(this)
-    this.canvas.onmousedown = this.mouseDownHandler.bind(this)
-    this.canvas.onmousemove = this.mouseMoveHandler.bind(this)
-  }
-
-  mouseUpHandler() {
-    this.mouseDown = false
-  }
-  mouseDownHandler(e: MouseEvent) {
-    this.mouseDown = true
-    this.ctx.beginPath()
-    this.startX = e.pageX - (e.target as HTMLCanvasElement).offsetLeft
-    this.startY = e.pageY - (e.target as HTMLCanvasElement).offsetTop
-    this.saved = this.canvas.toDataURL()
-  }
   mouseMoveHandler(e: MouseEvent) {
     if (this.mouseDown) {
       const currentX = e.pageX - (e.target as HTMLCanvasElement).offsetLeft
       const currentY = e.pageY - (e.target as HTMLCanvasElement).offsetTop
-
       const radius = Math.sqrt(
         Math.abs(this.startX - currentX) ** 2 +
           Math.abs(this.startY - currentY) ** 2
